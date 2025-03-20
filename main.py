7f256ab4b32acc606a6192f7dad5e67ce07aaa40
GabyDs <gabydsch2001@gmail.com>
2025-03-20 15:14:48 -0300

    format code

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from machine import Pin
 from time import sleep
 
 led_board = Pin("LED", Pin.OUT)
-sleep(1)    #le damos tiempo a vREPL
+
+sleep(1)  # le damos tiempo a vREPL
+
 print("\nLED esta destellando...")
+
 while True:
     try:
         led_board.toggle()
         # led_board.value(not led_board.value())
-        sleep(.5) # sleep 1sec
+        sleep(0.5)  # sleep 1sec
     except KeyboardInterrupt:
         break
+
 led_board.off()
+
 print("Listo")
